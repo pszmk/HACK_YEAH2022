5ee28b90dd7bd407209a359136b851cad4a9edb0
Gabriel-Konarski <gabrys.konarski@wp.pl>
2022-11-19 18:53:30 +0100

    item and category model

--- zero_waste/urls.py
+++ zero_waste/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', include("register.urls")),
-    path('items/', include("ownitems.urls")),
+    path('', include("ownitems.urls")),
     path('calendar/', include("calendarapp.urls")),
 ]
