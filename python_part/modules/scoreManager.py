8c5fef17fbe70cf84a672bff473d3a057b001dcf
pszmk <przemyslawpietrzak2001@gmail.com>
2022-11-20 10:42:20 +0100

    change name

--- python_part/modules/scoreManager.py
+++ python_part/modules/scoreManager.py
@@ -5,8 +5,9 @@ def resetScore():
     with open(Path(__file__).parent.parent / 'data' / 'user_score.json', 'w') as file:
         json.dump({"score": 0,"combo": 1}, file, indent=4)
 
-
 def add_subScore(score=0, add=True):
+    '''Adds or subtracts the score of the user.
+    '''
     with open(Path(__file__).parent.parent/'data'/'user_score.json', 'r') as file:
         file_list = json.load(file)
         if add:
